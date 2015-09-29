004682eedd53b1d3bba93bba43357803f046f3e1
Ulaize <ulaize@creative-tweed.com>
2015-09-29 17:52:33 +0000

    Exercise 1

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
-
-
 
 
 
@@ -18,3 +16,5 @@ var server = app.listen(process.env.PORT, process.env.IP, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
