8ad99c658663222cf9936b37c358d8b0ab05f223
Ulaize <ulaize@creative-tweed.com>
2015-09-29 19:00:19 +0000

    Exercise 3

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/op/:operation/:number1/:number2', function (req, res) {
+  var typeOp = req.params.operation;
+  var num1 = req.params.number1;
+  var num2 = req.params.number2;
+  
+  var mathOp;
+  switch(typeOp) {
+    case "add":
+        mathOp = Number(num1) + Number(num2);
+        break;
+    case "substract":
+        mathOp = Number(num1) - Number(num2);
+        break;
+    case "multiply":
+        mathOp = Number(num1) * Number(num2);
+        break;
+    case "divide":
+        mathOp = Number(num1) / Number(num2);
+        break;
+  }
+  
+  var result = {
+    operation: typeOp,
+    firstOperand: num1,
+    secondOperand: num2,
+    solution: mathOp
+  };
+  
+  res.json(result);
 });
-
-
-
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
